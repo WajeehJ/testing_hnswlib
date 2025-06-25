ecf5f9aaf00bfd5dc16f664fbd16f753b3a5c1c1
WajeehJ <wajeehdoesstuff@gmail.com>
2025-06-25 09:31:38 -0500

    hello

--- measuring_recall.py
+++ measuring_recall.py
@@ -1,60 +1,51 @@
 import hnswlib
 import numpy as np
+import time
 
-dim = 32
-num_elements = 100000
-k = 10
-nun_queries = 10
+# ---------- Utility to read .fvecs and .ivecs ----------
+def read_fvecs(file):
+    with open(file, 'rb') as f:
+        dim = np.fromfile(f, dtype=np.int32, count=1)[0]
+    return np.fromfile(file, dtype=np.float32).reshape(-1, dim + 1)[:, 1:]
 
-# Generating sample data
-data = np.float32(np.random.random((num_elements, dim)))
+def read_ivecs(file):
+    with open(file, 'rb') as f:
+        dim = np.fromfile(file, dtype=np.int32, count=1)[0]
+    return np.fromfile(file, dtype=np.int32).reshape(-1, dim + 1)[:, 1:]
 
-# Declaring index
-hnsw_index = hnswlib.Index(space='l2', dim=dim)  # possible options are l2, cosine or ip
-bf_index = hnswlib.BFIndex(space='l2', dim=dim)
+# ---------- Dataset Loading ----------
+print("Loading dataset...")
+base = read_fvecs("./datasets/sift_base.fvecs")[:1000000]
+queries = read_fvecs("./datasets/sift_query.fvecs")
+ground_truth = read_ivecs("./datasets/sift_groundtruth.ivecs")
 
-# Initing both hnsw and brute force indices
-# max_elements - the maximum number of elements (capacity). Will throw an exception if exceeded
-# during insertion of an element.
-# The capacity can be increased by saving/loading the index, see below.
-#
-# hnsw construction params:
-# ef_construction - controls index search speed/build speed tradeoff
-#
-# M - is tightly connected with internal dimensionality of the data. Strongly affects the memory consumption (~M)
-# Higher M leads to higher accuracy/run_time at fixed ef/efConstruction
+dim = base.shape[1]
+k = 1
+ef_values = list(range(10, 311, 30))
+num_threads_list = [1, 4, 8, 16]
 
-hnsw_index.init_index(max_elements=num_elements, ef_construction=200, M=16)
-bf_index.init_index(max_elements=num_elements)
+# ---------- Benchmark ----------
+for num_threads in num_threads_list:
+    print(f"\n--- Running with {num_threads} thread(s) ---")
 
-# Controlling the recall for hnsw by setting ef:
-# higher ef leads to better accuracy, but slower search
-hnsw_index.set_ef(200)
+    # Create HNSW index
+    index = hnswlib.Index(space='l2', dim=dim)
+    index.set_num_threads(num_threads)
+    t0 = time.time()
+    index.init_index(max_elements=len(base), ef_construction=200, M=32)
+    index.add_items(base)
+    build_time = time.time() - t0
+    print(f"Index built in {build_time:.2f} seconds.")
 
-# Set number of threads used during batch search/construction in hnsw
-# By default using all available cores
-hnsw_index.set_num_threads(1)
+    for ef in ef_values:
+        index.set_ef(ef)
 
-print("Adding batch of %d elements" % (len(data)))
-hnsw_index.add_items(data)
-bf_index.add_items(data)
+        start = time.time()
+        labels, _ = index.knn_query(queries, k=k)
+        end = time.time()
+        elapsed = end - start
 
-print("Indices built")
+        recall = np.mean([gt[0] in res for gt, res in zip(ground_truth, labels)])
+        qps = len(queries) / elapsed
 
-# Generating query data
-query_data = np.float32(np.random.random((nun_queries, dim)))
-
-# Query the elements and measure recall:
-labels_hnsw, distances_hnsw = hnsw_index.knn_query(query_data, k)
-labels_bf, distances_bf = bf_index.knn_query(query_data, k)
-
-# Measure recall
-correct = 0
-for i in range(nun_queries):
-    for label in labels_hnsw[i]:
-        for correct_label in labels_bf[i]:
-            if label == correct_label:
-                correct += 1
-                break
-
-print("recall is :", float(correct)/(k*nun_queries))
+        print(f"ef={ef:3d} | Recall@{k}={recall:.4f} | QPS={qps:.2f}")
